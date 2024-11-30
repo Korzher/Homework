65f9eb03585c001e55c3cbc55a25177e90a235e9
Korzher <tchuprinin.constantin@yandex.ru>
2024-11-30 23:22:00 +0300

    Guessing mechanism reworked

--- Guessing game.js
+++ Guessing game.js
@@ -1,18 +1,32 @@
-function magicNumber(min, max) {
-  const magicNumber = min + Math.random() * (max + 1 - min);
-  console.log("Компьютер 1: Я загадываю число");
-  return Math.floor(magicNumber);
+let secretNumber;
+let guess;
+var min = 1;
+var max = 100;
+
+function magicNumber() {
+  secretNumber = Math.floor(min + Math.random() * (max + 1 - min));
+  console.log(`Компьютер 1 (по секрету): Я загадываю число ${secretNumber}`);
+  return guessing(min, max);
 }
 
 function guessing(min, max) {
-  let guess = Math.floor((min + max) / 2);
+  guess = Math.floor((min + max) / 2);
   console.log(`Компьтер 2: Пробую число ${guess}`);
   return check(guess);
 }
 
 function check(guess) {
-  return;
+  if (guess > secretNumber) {
+    max = guess;
+    console.log("Компьютер 1: Меньше");
+    return guessing(min, max);
+  } else if (secretNumber > guess) {
+    min = guess;
+    console.log("Компьютер 1: Больше");
+    return guessing(min, max);
+  } else {
+    return console.log("Компьютер 1: Угадал!");
+  }
 }
 
-magicNumber(1, 100);
-guessing(1, 100);
+magicNumber();
