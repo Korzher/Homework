b65ffc0429abd54ec0731d97203ce8b70e2bf3ce
Korzher <tchuprinin.constantin@yandex.ru>
2025-01-01 12:18:13 +0300

    Rework of mySlice function

--- TaskN9.js
+++ TaskN9.js
@@ -9,46 +9,16 @@ function mySlice(arr, start, end) {
   let result = [];
   let counter = 0;
 
-  if (!start) {
-    return arr;
-  } else if (start >= 0) {
-    if (!end) {
-      end = arr.length;
-    }
+  start = start ? start : 0;
+  end = end ? end : arr.length;
+  start = start >= 0 ? start : arr.length + start;
+  end = end >= 0 ? end : arr.length + end;
 
-    if (end >= 0) {
-      for (let i = start; i < end; i++) {
-        result[counter] = arr[i];
-        counter++;
-      }
-    } else if (end < 0) {
-      for (let i = start; i < arr.length + end; i++) {
-        result[counter] = arr[i];
-        counter++;
-      }
-    }
-  } else {
-    if (!end) {
-      for (let i = start; i < 0; i++) {
-        if (arr[arr.length + i]) {
-          result[counter] = arr[arr.length + i];
-          counter++;
-        }
-      }
-    } else {
-      if (end < 0) {
-        for (let i = start; i < end; i++) {
-          result[counter] = arr[arr.length + i];
-          counter++;
-        }
-      } else {
-        for (let i = arr.length + start; i < end; i++) {
-          result[counter] = arr[i];
-          counter++;
-        }
-      }
-    }
+  for (let i = start; i < end; i++) {
+    result[counter] = arr[i];
+    counter++;
   }
+
   return result;
 }
 
@@ -95,8 +65,8 @@ function myIncludes(arr, item, from) {
 }
 
 let arr = ["t", "e", "s", "t"];
-console.log(mySlice(arr, 1, 3)); // e,s (копирует с 1 до 3)
-console.log(mySlice(arr, -2)); // s,t (копирует с -2 до конца)
+mySlice(arr, 1, 3); // e,s (копирует с 1 до 3)
+mySlice(arr, -2); // s,t (копирует с -2 до конца)
 
 let arrIndexOf = [1, 0, false];
 console.log(myIndexOf(arrIndexOf, 0)); // 1
@@ -110,3 +80,20 @@ console.log(myIndexOf(fruits, "Яблоко")); // 0 (первый 'Яблоко
 const arrNaN = [NaN];
 console.log(myIndexOf(arrNaN, NaN)); // -1 (неверно, должен быть 0)
 console.log(myIncludes(arrNaN, NaN)); // true (верно)
+
+//Немного  ручных тестов
+arrSlice = [0, 1, 2, 3, 4, 5];
+console.log(mySlice(arrSlice, 1, 3));
+console.log(arrSlice.slice(1, 3));
+console.log(mySlice(arrSlice, -4, -2));
+console.log(arrSlice.slice(-4, -2));
+console.log(mySlice(arrSlice, -4, 3));
+console.log(arrSlice.slice(-4, 3));
+console.log(mySlice(arrSlice, 1, -1));
+console.log(arrSlice.slice(1, -1));
+console.log(mySlice(arrSlice, 1));
+console.log(arrSlice.slice(1));
+console.log(mySlice(arrSlice));
+console.log(arrSlice.slice());
+console.log(mySlice(arrSlice, 8, -4));
+console.log(arrSlice.slice(8, -4));
